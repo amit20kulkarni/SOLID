07c01f0201d5ab53e66e736538c97cb919235c34
AMIT KULKARNI <amitshaiwale20@gmail.com>
2024-01-15 17:32:32 +0530

    updates

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,26 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FormValidationComponent } from './examples/form-validation/form-validation.component';
+import { LandingPageComponent } from './landing-page/landing-page.component';
+import { NavigationComponent } from './navigation/navigation.component';
+import { LandingContentComponent } from './landing-content/landing-content.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FormValidationComponent,
+    LandingPageComponent,
+    NavigationComponent,
+    LandingContentComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
